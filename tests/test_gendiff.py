3a967922c94bbfb7d8ca991981019aa92626c1d4
Игорь Рудич <rudi4.clarinet@mail.ru>
2023-09-27 15:48:54 +0300

    add test_type_result

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -9,3 +9,8 @@ def test_similar_files():
 def test_different_files_1():
     result = generate_diff('test_file3.json', 'test_file1.json', files_directory='tests/fixtures/')
     assert result == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+
+
+def test_type_result():
+    result = generate_diff('test_file3.json', 'test_file1.json', files_directory='tests/fixtures/')
+    assert type(result) == str
