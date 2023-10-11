be52caddbfe35c15cd59cea371e736ad99a24d6e
Игорь Рудич <rudi4.clarinet@mail.ru>
2023-10-11 12:47:30 +0300

    remove complex value from plain

--- gendiff/styles/plain.py
+++ gendiff/styles/plain.py
@@ -18,15 +18,15 @@ def get_string(data, string=''):
             return f"Property '{string}' was removed\n"
         elif 'diff types values' in data:
             if type(data[0]) == dict:
-                if check_value(data[1]):                
-                    return f"Property '{string}' was updated. From [complex value] to '{data[1]}'\n"
+                if check_value(data[1]):                            #!!!!
+                    return f"Property '{string}' was updated. From '{data[0]}' to '{data[1]}'\n"
                 else:
-                    return f"Property '{string}' was updated. From [complex value] to {data[1]}\n"
+                    return f"Property '{string}' was updated. From '{data[0]}' to {data[1]}\n"
             elif type(data[1]) == dict:
-                if check_value(data[0]):
-                    return f"Property '{string}' was updated. From '{data[0]}' to [complex value]\n"
+                if check_value(data[0]):                                           #!!!!
+                    return f"Property '{string}' was updated. From '{data[0]}' to {data[1]}\n"
                 else:
-                    return f"Property '{string}' was updated. From {data[0]} to [complex value]\n"
+                    return f"Property '{string}' was updated. From '{data[0]}' to {data[1]}\n"
         elif 'not dict and diff' in data:
             if check_value(data[0]) == True and check_value(data[1]) == True:
                 return f"Property '{string}' was updated. From '{data[0]}' to '{data[1]}'\n"
