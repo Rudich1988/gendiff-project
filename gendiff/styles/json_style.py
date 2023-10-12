b9121e2b706cb55a56496e04c19428ddc402c06d
Игорь Рудич <rudi4.clarinet@mail.ru>
2023-10-12 22:55:14 +0300

    change logic in json_style.py

--- gendiff/styles/json_style.py
+++ gendiff/styles/json_style.py
@@ -1,5 +1,7 @@
 from gendiff.styles.data_conversion import data_conversion
 
+INDENT_QUANTITY = 4
+
 
 def equality_check(files_data):
     flag = True
@@ -20,18 +22,67 @@ def check(value):
             return f'"{value}"'
     return value
 
-
+'''
 def get_comma(flag):
     if flag is False:
         return ','
     return ''
+'''
+'''
+def get_json_format_line(files_data, enclosure):
+    finish_string = ''
+    if equality_check(files_data) is True:
+        count = 0
+        for key, value in files_data.items():
+            flag = False
+            count += 1
+            if count == len(files_data) and len(files_data) != 1:
+                flag = True
+            finish_string += (INDENT_QUANTITY * enclosure) * ' ' + f'"{key}"' + ': ' + final_stylish(value, enclosure=enclosure + 1) + get_comma(flag) + '\n'
+        finish_string += (INDENT_QUANTITY * (enclosure - 1)) * ' ' + '}'
+    return finish_string
+'''
 
 
-def final_stylish(files_data, indent_quantity=4, enclosure=1):
+
+#!!!
+def ge_comma(number, len_data):
+    if len_data == number:
+        return ''
+    return ','
+
+
+
+
+def final_style(files_data, enclosure=1, flag=True):
     if type(files_data) is not dict:
         return str(check(files_data))
     else:
         finish_string = '{\n'
+        count = 0
+        for key, value in files_data.items():
+            count += 1
+            possible_comma = ge_comma(count, len(files_data))
+            if key[:2] != '- ' and key[:2] != '+ ':
+                finish_string += (INDENT_QUANTITY * enclosure) * ' ' + f'"{key}"' + ': ' + final_style(value, enclosure=enclosure + 1) + ge_comma(count, len(files_data)) + '\n'
+            else:
+                finish_string += (INDENT_QUANTITY * enclosure - 2) * ' ' + f'"{key}"' + ': ' + final_style(value, enclosure=enclosure + 1) + ge_comma(count, len(files_data)) + '\n'
+        finish_string += (INDENT_QUANTITY * (enclosure - 1)) * ' ' + '}'
+        return finish_string
+#!!!
+
+
+
+
+
+
+'''
+def final_stylish(files_data, enclosure=1):
+    if type(files_data) is not dict:
+        return str(check(files_data))
+    else:
+        finish_string = '{\n'
+        #finish_string += get_json_format_line(files_data, enclosure)
         if equality_check(files_data) is True:
             count = 0
             for key, value in files_data.items():
@@ -39,8 +90,8 @@ def final_stylish(files_data, indent_quantity=4, enclosure=1):
                 count += 1
                 if count == len(files_data):
                     flag = True
-                finish_string += (indent_quantity * enclosure) * ' ' + f'"{key}"' + ': ' + final_stylish(value, enclosure=enclosure + 1) + get_comma(flag) + '\n'
-            finish_string += (indent_quantity * (enclosure - 1)) * ' ' + '}'
+                finish_string += (INDENT_QUANTITY * enclosure) * ' ' + f'"{key}"' + ': ' + final_stylish(value, enclosure=enclosure + 1) + get_comma(flag) + '\n'
+            finish_string += (INDENT_QUANTITY * (enclosure - 1)) * ' ' + '}'
             return finish_string
         count = 0
         for key, value in files_data.items():
@@ -49,12 +100,13 @@ def final_stylish(files_data, indent_quantity=4, enclosure=1):
             if count == len(files_data) and len(files_data) != 1:
                 flag = True
             if key[:2] != '- ' and key[:2] != '+ ':
-                finish_string += (indent_quantity * enclosure) * ' ' + f'"{key}"' + ': ' + final_stylish(value, enclosure=enclosure + 1) + get_comma(flag) + '\n'
+                finish_string += (INDENT_QUANTITY * enclosure) * ' ' + f'"{key}"' + ': ' + final_stylish(value, enclosure=enclosure + 1) + get_comma(flag) + '\n'
             else:
-                finish_string += (indent_quantity * enclosure - 2) * ' ' + f'"{key}"' + ': ' + final_stylish(value, enclosure=enclosure + 1) + get_comma(flag) + '\n'
-        finish_string += (indent_quantity * (enclosure - 1)) * ' ' + '}'
+                finish_string += (INDENT_QUANTITY * enclosure - 2) * ' ' + f'"{key}"' + ': ' + final_stylish(value, enclosure=enclosure + 1) + get_comma(flag) + '\n'
+        finish_string += (INDENT_QUANTITY * (enclosure - 1)) * ' ' + '}'
     return finish_string
-
+'''
 
 def stylish_12(data):
-    return final_stylish(data_conversion(data))
+    return final_style(data_conversion(data))
+    #return final_stylish(data_conversion(data))
